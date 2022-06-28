fb1f3a61b8d1d535b5f80fd14226843658019a9a
EC2 Default User <kazukipump7@gmail.com>
2022-06-28 19:40:23 +0000

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 200 }
   validates :title, presence: true
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: :about
-  resources :books, only:[:index,:show,:create,:edit,:update,:destroy]
+  resources :books, only:[:index,:show,:create,:edit,:update,:destroy] do
+  resources :book_comments, only:[:create,:destroy]
+    resource :favorites,only:[:create, :destroy]
+  end
   resources :users, only:[:show,:index,:edit,:update]
 
 end
