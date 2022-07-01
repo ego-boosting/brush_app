7cb2c8d64b7a054920a1650fabcf26a9cb71af2e
EC2 Default User <kazukipump7@gmail.com>
2022-07-01 17:19:18 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,29 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foregin_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foregin_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, throuth: :relationships, source: :followed
+  has_many :followers, throuth: :relationships, source: :follower
+
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction,  length: { maximum: 50 }
+  
+    # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 
   def get_profile_image(width,height)
     unless profile_image.attached?
