178af302607ec96695d4caf6a5da805306144750
EC2 Default User <kazukipump7@gmail.com>
2022-07-02 09:08:22 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,16 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   # フォローをした、されたの関係
-  has_many :relationships, class_name: "Relationship", foregin_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foregin_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   # 一覧画面で使う
-  has_many :followings, throuth: :relationships, source: :followed
-  has_many :followers, throuth: :relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction,  length: { maximum: 50 }
-  
+
     # フォローしたときの処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
